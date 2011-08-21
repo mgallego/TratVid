4bd700a45605a5617d0a2a7b92e82c9bc23d54a4
Moises Gallego <moisesgallego@gmail.com>
2011-08-21 13:10:41 +0200

    initial commit

---
+++ tratvid.py
@@ -0,0 +1,60 @@
+from  glob import iglob
+import shutil
+import os
+import sys
+import ConfigParser
+import time
+
+def ConvertirFichero(fichero):
+	os.system("ffmpeg -i " + fichero + " -an -sameq -y -s "+ resolucion+" "+ rutaTemporal + fichero + extensionSalida)
+
+def BorrarTemporal():
+	shutil.rmtree(rutaTemporal)
+
+def CrearRutaTemporal():
+	if not os.path.exists(rutaTemporal):
+		os.mkdir(rutaTemporal)
+
+def CrearRutaFinal():
+	if not os.path.exists(rutaFinal):
+		os.mkdir(rutaFinal)
+
+def ConvertirFicherosDelDirectorio():
+	for name in os.listdir(sys.argv[1]):
+		ConvertirFichero(name)
+	
+def CrearFicheroFinal():
+	fecha = time.localtime()
+
+	#refactorizar
+	ficheroFinal = str(fecha[0]) + str(fecha[1]) + str(fecha[2]) + str(fecha[3]) + str(fecha[4]) + str(fecha[5]) + ".avi"
+
+	destino = open(rutaTemporal + 'salidaTemp','wb')
+
+	for filename in iglob(os.path.join(rutaTemporal,'*.avi')):
+		#print filename
+		shutil.copyfileobj(open(filename,'rb'), destino)
+
+	destino.close()
+
+	os.system("ffmpeg -i " + rutaTemporal + "salidaTemp -sameq " + rutaFinal + ficheroFinal)
+
+
+cfg = ConfigParser.ConfigParser()
+if not cfg.read(["./tratvid.cfg"]):
+	print "No existe el archivo de configuracion"	
+
+resolucion      = cfg.get("video","resolucion")
+rutaTemporal    = os.getcwd() + cfg.get("rutas","temporal")
+rutaFinal		= os.getcwd() + cfg.get("rutas","final")
+extensionSalida = cfg.get("salida","extension")
+
+CrearRutaTemporal()
+
+CrearRutaFinal()
+
+ConvertirFicherosDelDirectorio()
+
+CrearFicheroFinal()
+
+BorrarTemporal()
