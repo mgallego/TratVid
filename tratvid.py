ef23010a81606db297a1e4732c59fa39316d0e41
Moises Gallego <moisesgallego@gmail.com>
2011-08-21 15:20:12 +0200

    closed #2 y #3

--- tratvid.py
+++ tratvid.py
@@ -6,39 +6,60 @@ import ConfigParser
 import time
 
 def ConvertirFichero(fichero):
-	os.system("ffmpeg -i " + rutaActual + "/" + fichero + " -an -sameq -y -s "+ resolucion+" "+ rutaTemporal + fichero + extensionSalida)
+	print "Convirtiendo el fichero: " + rutaActual + "/" + fichero
+
+	os.system("ffmpeg -i " + rutaActual + "/" + fichero + " -an -sameq -y -s "+ resolucion+" "+ rutaTemporal + fichero + extensionSalida + " 2>> " + rutaLogs + ficheroFinal + ".log")
 
 def BorrarTemporal():
 	shutil.rmtree(rutaTemporal)
 
 def CrearRutaTemporal():
 	if not os.path.exists(rutaTemporal):
+		print "Creando el directorio temporal"
 		os.mkdir(rutaTemporal)
 
 def CrearRutaFinal():
 	if not os.path.exists(rutaFinal):
+		print "creando el directorio final"
 		os.mkdir(rutaFinal)
+
+def CrearRutaLogs():
+	if not os.path.exists(rutaLogs):
+		print "creando el directorio de logs"
+		os.mkdir(rutaLogs)	
+
+def CrearCarpetas():
+	CrearRutaTemporal()
+	CrearRutaFinal()
+	CrearRutaLogs()
 
 def ConvertirFicherosDelDirectorio():
 	for name in os.listdir(rutaActual):
 		ConvertirFichero(name)
 	
 def CrearFicheroFinal():
-	fecha = time.localtime()
-
-	#refactorizar
-	ficheroFinal = str(fecha[0]) + str(fecha[1]) + str(fecha[2]) + str(fecha[3]) + str(fecha[4]) + str(fecha[5]) + ".avi"
-
-	destino = open(rutaTemporal + 'salidaTemp','wb')
+	print "Creando el fichero final: " + rutaFinal + ficheroFinal
+	
+	destino = open(rutaTemporal + ficheroTemporal,'wb')
 
 	for filename in iglob(os.path.join(rutaTemporal,'*.avi')):
-		#print filename
 		shutil.copyfileobj(open(filename,'rb'), destino)
 
 	destino.close()
 
-	os.system("ffmpeg -i " + rutaTemporal + "salidaTemp -sameq " + rutaFinal + ficheroFinal)
+	os.system("ffmpeg -i " + rutaTemporal + ficheroTemporal + " -sameq " + rutaFinal + ficheroFinal + extensionSalida + " 2>> " + rutaLogs + ficheroFinal + ".log")
 
+
+def NombreFinal():
+	fecha = time.localtime()
+
+	ficheroFinal = ''	
+
+	for i in range(6):
+		ficheroFinal = ficheroFinal + str(fecha[i])
+	#ficheroFinal = ficheroFinal + ".avi"
+
+	return ficheroFinal	
 
 cfg = ConfigParser.ConfigParser()
 if not cfg.read(["./tratvid.cfg"]):
@@ -48,11 +69,14 @@ rutaActual		= os.path.realpath(sys.argv[1])
 resolucion      = cfg.get("video","resolucion")
 rutaTemporal    = rutaActual + cfg.get("rutas","temporal")
 rutaFinal		= rutaActual + cfg.get("rutas","final")
+rutaLogs		= rutaActual + cfg.get("rutas","logs")
 extensionSalida = cfg.get("salida","extension")
+ficheroTemporal = cfg.get("salida","ficherotemporal")
+ficheroFinal 	= NombreFinal()
 
-CrearRutaTemporal()
+NombreFinal()
 
-CrearRutaFinal()
+CrearCarpetas()
 
 ConvertirFicherosDelDirectorio()
 
